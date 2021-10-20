45b492e97955bd562bee39e4e19c23695f014807
aref yazdanpanah <arefyazdanpanah4421@gmail.com>
2021-10-20 20:49:51 +0330

    x.py and x2.py commit to repo

---
+++ x.py
@@ -0,0 +1,6 @@
+def sum_of_numbers(n) :
+    result = 0
+    return result
+n = input("Please Enter Your number\t")
+sum = sum_of_numbers(int(n))
+print("Sum Of This Number Is: ",sum)
---
+++ x2.py
@@ -0,0 +1,5 @@
+def sum_of_numbers(n) :
+	pass
+n = input("Please Enter Your number\t")
+sum = sum_of_numbers(int(n))
+print("Sum Of This Number Is: ",sum)
